fed8a461a82111f1c4cc6ae4144e95ad5a14f156
Felipe Daniel <felipedanielss@msn.com>
2022-07-08 11:14:23 -0300

    nova versao 1.5
    1.5

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ console.log(date.format(new Date()));
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+
+//*código para contador de visitas*//
+
+<a href="https://www.webcontadores.com" title="contador de visitas"><img src="https://counter9.stat.ovh/private/webcontadores.php?c=5sdk26uh3r8rcq1sxqjypmg8ftdbwfdk" border="0" title="contador de visitas" alt="contador de visitas"></a>
+
+
+
+
+
+
+
+
+
+
